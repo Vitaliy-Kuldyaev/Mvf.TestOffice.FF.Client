75a01b8d2fe1347689fa9e3dcae8d52bece855c0
Vitaliy-Kuldyaev <78350162+Vitaliy-Kuldyaev@users.noreply.github.com>
2022-10-03 12:05:51 +0200

    Add files via upload
    first commit

---
+++ Mvf.TestOffice.FF.Client/Mvf.TestOffice.FF.Client/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mvf.TestOffice.FF.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //DXMessageBox.Show(caption: "Dialog Header", messageBoxText: "This is your message", button: MessageBoxButton.OK, icon: MessageBoxImage.Exclamation);
+            ThemedMessageBox.Show(title: "", text: "Данные успешно записаны", messageBoxButtons: MessageBoxButton.OK, icon: MessageBoxImage.Exclamation);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
